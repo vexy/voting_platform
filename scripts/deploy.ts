eff1fd6c99217346bb4b6111f5da15849fbdcb1b
Veljko Tekelerović <veljko.tekelerovic@gmail.com>
2024-01-19 21:53:45 +0100

    Updated deploy script and it's output

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,26 +2,27 @@ import { ethers } from "hardhat";
 import hre from "hardhat";
 
 async function main() {
-  console.log(":: Compiling contracts");
+  console.log("\x1b[42m\x1b[37m:: Contract compilation\x1b[0m");
+  console.log("- \x1b[33mCompiling contracts...\x1b[0m");
   await hre.run('compile');
-  console.log(":: Compilation completed.\n");
+  console.log(":: Contract compilation \x1b[32m\x1b[1mcompleted.\x1b[0m\n");
 
   const platformFactory = await ethers.getContractFactory("MainPlatform");
   const baseOwner = (await ethers.getSigners())[0].address;
 
   // setup deployment and transaction holder
-  const platform = await platformFactory.deploy(baseOwner);
-  const depTxHash = platform.deployTransaction.hash;
+  console.log("\x1b[42m\x1b[37m:: Contract deployment\x1b[0m");
+  console.log("- \x1b[33mDeploying contract...\x1b[0m");
+  const mainPlatform = await platformFactory.deploy(baseOwner);
+  await mainPlatform.waitForDeployment()
+  //
+  const contractAddress = await mainPlatform.getAddress();
+  const depTxHash = mainPlatform.deploymentTransaction();
 
-  // wait for actual deployment
-  console.log(":: Contract deployment");
-  await platform.deployed();
-  console.log("- Deployment completed, awaiting receipt...");
-  const receipt = await ethers.provider.waitForTransaction(depTxHash);
-  console.log("-- Receipt:");
-  console.log("-- MainPlatform address:", receipt.contractAddress);
-  console.log("-- Contract owner:\t", receipt.from);
-  console.log(":: Deployment completed");
+  console.log("- Deployment completed, results:");
+  console.log(`-- Contract [MainPlatform] address: \x1b[1m\x1b[34m${contractAddress}\x1b[0m`);
+  console.log(`-- Deployment transaction  hash: \x1b[1m\x1b[34m${depTxHash?.hash}\x1b[0m`);
+  console.log(":: Contract deployment \x1b[32m\x1b[1mcompleted.\x1b[0m");
 }
 
 // Execute the script...
